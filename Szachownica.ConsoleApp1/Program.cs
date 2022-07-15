1ae7f7fd8598f0a42ab86235cedd23f45e08bfb7
Tomek <tomoj.metlika@gmail.com>
2022-07-15 18:09:51 +0200

    szachownica pętla w pętli 4o min roboty

---
+++ Szachownica.ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("SZACHOWNICA!");
+
+for (int i = 1; i <=1 ;i++ )
+
+    Console.WriteLine("01010101");
+    Console.WriteLine("10101010");
+{
+    for (int j = 1; j <= 3; j++)
+    {
+        Console.WriteLine("01010101");
+        Console.WriteLine("10101010");
+    }
+
+
+}    
+
+   
