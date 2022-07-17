4c0d6be4da35be3d3173b517f193ffb1bec3cd9e
Tomek <tomoj.metlika@gmail.com>
2022-07-17 15:21:49 +0200

    pętla modulo

--- Szachownica.ConsoleApp1/Program.cs
+++ Szachownica.ConsoleApp1/Program.cs
@@ -2,9 +2,9 @@
 Console.WriteLine("Hello World!");
 
 for (int i = 1; i <=1 ;i++ )
-
+{ 
     Console.WriteLine("SZACHOWNICA!");
-{
+
     for (int j = 1; j <= 4; j++)
     {
         Console.WriteLine("10101010");
@@ -18,3 +18,43 @@ for (int i = 1; i <=1 ;i++ )
 {
 Console.WriteLine("Będę Programistą");
 }
+
+
+
+ 
+
+    for (int i = 0; i < 8; i++)
+    {
+        
+          for (int x =0 ; x<8 ;x++) 
+            {
+                Console.Write((i+x)%2);  
+               
+                
+            }
+
+    Console.WriteLine();
+
+     }
+
+
+
+//pętla z palca
+Console.WriteLine("Pętla z palca");
+for (int i = 0; i < 8; i++)
+{
+    for (int x = 0; x < 8; x++)
+    {
+        Console.Write((i + x) % 2);
+
+       
+        if (i + x == 14)  //warunek w pętli
+        {
+            Console.WriteLine();
+            Console.WriteLine("Tomek");
+
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Sukces");
