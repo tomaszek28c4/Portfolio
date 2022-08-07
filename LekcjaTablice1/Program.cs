a3fcc4651602c231bad8a46ce078c0a949f00010
Tomek <tomoj.metlika@gmail.com>
2022-08-07 11:14:24 +0200

    tablice lekcja 1

---
+++ LekcjaTablice1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Tablice1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int[] tablica = new int[365];//tablica musi miec z gory przyjety w nawiasach [] rozmiar
+            string[] dniTygodnia = /* tablica typu string */
+                { "poniedziałek", "wtorek", "sroda", "czwartek",
+                 "piatek", "sobota", "niedziela" };
+
+            for (int i = 0; i < dniTygodnia.Length; i++)
+            {
+                System.Console.WriteLine(dniTygodnia[i]);
+            }
+
+            program tab = new program();
+            tab.tablicaDruga();
+
+        }
+
+        public void tablicaDruga()
+        {
+            int[,] tablica = new int [2,3] ;
+            tablica[0, 0] = 2;
+            tablica[0, 1] = 3;
+            tablica[0, 2] = 4;
+            tablica[1, 0] = 5;
+            tablica[1, 1] = 6;
+            tablica[1, 2] = 7;
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < tablica.GetLength(1); j++)
+                {
+                    System.Console.Write(tablica [i, j] +  "  ");
+                }
+                System.Console.WriteLine();
+            }
+
+        }
+
+    }
+
+}
--- Metody_Funkcje_2/Program.cs
+++ Metody_Funkcje_2/Program.cs
@@ -1,20 +1,23 @@
-﻿namespace MetodyFunkcje
+﻿namespace MetodyFunkcje2
 {
     class program
     {
         public static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             square s = new square();
             s.PoleKwadrata();
-
             triangle t = new triangle();
             t.Trojkat();
+            t.ObwodTrk();
         }
 
         class square
         {
-            public  void PoleKwadrata()
+            public void PoleKwadrata()
             {
+                Console.WriteLine();
+                Console.WriteLine("Pole kwadratu");
                 string bokA;
                 string bokB;
                 double user1;
@@ -27,15 +30,20 @@
                 double.TryParse(bokA, out user1);
                 double.TryParse(bokB, out user2);
                 pole = user1 * user2;
+                
                 Console.WriteLine("Pole Kwadratu wynosi   :" + pole);
+               
+                Console.WriteLine();
             }
         }
 
         class triangle
         {
 
-            public  void Trojkat()
+            public void Trojkat()
             {
+                Console.WriteLine();
+                Console.WriteLine("Pole trójkąta");
                 string wysokosc;
                 string podstawa;
                 double user1;
@@ -49,7 +57,32 @@
                 double.TryParse(podstawa, out user2);
                 pole = user1 * user2;
                 Console.WriteLine("Pole trójkąta wynosi   :" + pole);
+                Console.WriteLine();
 
+            }
+            public void ObwodTrk()
+            {
+                Console.WriteLine();
+                Console.WriteLine("Obwód trójkąta");
+                string bokA;
+                string bokB;
+                string bokC;
+                double user1;
+                double user2;
+                double user3;
+                double obwod;
+                Console.WriteLine("Podaj długość boku a");
+                bokA = Console.ReadLine();
+                Console.WriteLine("Podaj długość boku b");
+                bokB = Console.ReadLine();
+                Console.WriteLine("Podaj długość boku c");
+                bokC = Console.ReadLine();
+                double.TryParse(bokA, out user1);
+                double.TryParse(bokB, out user2);
+                double.TryParse(bokC, out user3);
+                obwod = user1 + user2 + user3;
+                Console.WriteLine("obwod trojkąta wynosi    :" + obwod);
+                Console.WriteLine();
             }
         }
 
