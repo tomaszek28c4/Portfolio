d70da0638862a062f1761635cd94ae288745d743
Tomek <tomoj.metlika@gmail.com>
2022-09-28 20:53:07 +0200

    dodanie listy generycznej

--- SZACHOWNICA2/Program.cs
+++ SZACHOWNICA2/Program.cs
@@ -1,31 +1,54 @@
-﻿using System;
-
-namespace Szachownica2
+﻿namespace Szachownica2
 {
-    class Rectangel
+    abstract public class Rectangel
     {
-        public static void Rect(int a, int b, char borderCh, char fillCh)
+       virtual public void Rect(int a, int b, char borderCh, char fillCh)
         {
             for (int i = 0; i < a; i++)
             {
                 for (int j = 0; j < b; j++)
                 {
-                    if (i==0 || i == a - 1 || j==0 || j == b - 1)
+                    if (i == 0 || i == a - 1 || j == 0 || j == b - 1)
                     {
                         Console.Write(borderCh);
                     }
-                    else 
+                    else
                         Console.Write(fillCh);
                 }
                 Console.WriteLine();
             }
 
         }
+        char borderCh;
+        char fillCh;
+
+
     }
 
-    class Chess
+    class Rectangel1 : Rectangel
     {
-        public static void Ches()
+        public override void Rect(int a, int b, char borderCh, char fillCh)
+        {
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (i == 0 || i == a - 1 || j == 0 || j == b - 1)
+                    {
+                        Console.Write(borderCh);
+                    }
+                    else
+                        Console.Write(fillCh);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Chess<T>
+    {
+        public void Ches(T parametr)
         {
             for (int i = 0; i < 8; i++)
             {
@@ -52,14 +75,14 @@ namespace Szachownica2
                     if (i == 0 || i == a - 1 || j == 0 || j == b - 1)
                     {
                         Console.Write(borderCh);
-                       
+
                     }
                     else
                         Console.Write(fillCh);
 
                 }
                 Console.WriteLine();
-              
+
             }
             Console.WriteLine();
             for (int k = 0; k < 5; k++)
@@ -80,13 +103,26 @@ namespace Szachownica2
     {
         public static void Main(string[] args)
         {
-           Rectangel.Rect(5, 100, '#', '*');
-            Console.WriteLine();
-            Chess.Ches();
-            Console.WriteLine();
-            Mix.SetMix(5 , 5 , '0' , '1');
-            Console.WriteLine();
-            Console.ReadKey();
+            // Rectangel.Rect(5, 100, '#', '*');
+            // Console.WriteLine();
+            // Chess.Ches();
+            // Console.WriteLine();
+            // Mix.SetMix(5 , 5 , '0' , '1');
+            // Console.WriteLine();
+            //Console.ReadKey();
+
+            // Chess<int> chess1 = new Chess<int>();
+            // chess1.Ches(1);
+            List<Rectangel> rect1 = new List<Rectangel>()
+            {
+                new Rectangel1()
+            };
+
+
+            foreach( Rectangel rectangel in rect1)
+            {
+                rectangel.Rect(10,10,'#','*');
+            }
         }
 
     }
