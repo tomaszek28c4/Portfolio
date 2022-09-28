533db8b8ccae1939812848b31075be18217d22a4
Tomek <tomoj.metlika@gmail.com>
2022-09-28 18:40:31 +0200

    pętle for oraz funkcja if else

--- Generyzm2/Program.cs
+++ Generyzm2/Program.cs
@@ -1,51 +1,51 @@
-﻿using System;
-
-namespace Generyzm2
+﻿namespace Generyzm2
 {
+
+
+
+    public class KlasaGeneryczna<T>
+    {
+        public void Dodaj(T parametr)
+
+        {
+            Console.WriteLine("Podaj wartość");
+            string wartosc;
+            wartosc = Console.ReadLine();
+            Console.WriteLine($"Wynik metody Dodaj:  + {parametr} ");
+        }
+    }
+
+    public class Generyzm<T>
+    {
+        public void Wartosc(T parametr)
+        {
+            Console.WriteLine("Podaj Wartość");
+            string wartosc;
+            wartosc = Console.ReadLine();
+            Console.WriteLine($" wartość string + paramert = {wartosc}  " + parametr);
+        }
+
+    }
 
     class Program
     {
 
         static void Main(string[] args)
         {
-           
-           // KlasaGeneryczna<int> obiektKlasyGenerycznej = new KlasaGeneryczna<int>();
-           // obiektKlasyGenerycznej.Dodaj(12); // 12 to wartość parametru int dla metody Dodaj w klasie generycznej
 
-           // KlasaGeneryczna<string> obiektKlasyGenerycznej1 = new KlasaGeneryczna<string>();
-           // obiektKlasyGenerycznej1.Dodaj("PARAMETR");
+             KlasaGeneryczna<int> obiektKlasyGenerycznej = new KlasaGeneryczna<int>();
+             obiektKlasyGenerycznej.Dodaj(12); // 12 to wartość parametru int dla metody Dodaj w klasie generycznej
+
+             KlasaGeneryczna<string> obiektKlasyGenerycznej1 = new KlasaGeneryczna<string>();
+             obiektKlasyGenerycznej1.Dodaj("PARAMETR");
 
 
-           // KlasaGeneryczna<string> obiektKlasyGenerycznej2 = new KlasaGeneryczna<string>();
-           // obiektKlasyGenerycznej2.Dodaj("Wartość parametru string dla metody Dodaj w KlasieGenerycznej");
+            KlasaGeneryczna<string> obiektKlasyGenerycznej2 = new KlasaGeneryczna<string>();
+            obiektKlasyGenerycznej2.Dodaj("Wartość parametru dla metody Dodaj w KlasieGenerycznej");
 
-           Generyzm<string> obiektKlasyGeneryzm = new Generyzm<string>();
-            obiektKlasyGeneryzm.Wartosc("Zawsze MUSI BYĆ podany PARAMETR");
+            Generyzm<string> obiektKlasyGeneryzm = new Generyzm<string>();
+            obiektKlasyGeneryzm.Wartosc ("Zawsze MUSI BYĆ podany PARAMETR");
             Console.ReadKey();
-        }
-
-        public class KlasaGeneryczna<T>
-        {
-            public void Dodaj(T parametr)
-
-            {
-              // Console.WriteLine("Podaj wartość");
-              // string  wartosc;
-              // wartosc = Console.ReadLine();
-              //Console.WriteLine($"Wynik metody Dodaj:  + {parametr} ");
-            }
-        }
-
-        public class Generyzm<T>
-        {
-           public void Wartosc(T parametr)
-            {
-                Console.WriteLine("Podaj Wartość");
-                string wartosc;
-                wartosc = Console.ReadLine();
-                Console.WriteLine($" wartość + paramert +   { wartosc}" + parametr);
-            }
-
         }
 
     }
---
+++ SZACHOWNICA2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Szachownica2
+{
+    class Rectangel
+    {
+        public static void Rect(int a, int b, char borderCh, char fillCh)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (i==0 || i == a - 1 || j==0 || j == b - 1)
+                    {
+                        Console.Write(borderCh);
+                    }
+                    else 
+                        Console.Write(fillCh);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+
+    class Chess
+    {
+        public static void Ches()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Console.Write((i + x) % 2);
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+
+    public class Mix
+    {
+        public static void SetMix(int a, int b, char borderCh, char fillCh)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (i == 0 || i == a - 1 || j == 0 || j == b - 1)
+                    {
+                        Console.Write(borderCh);
+                       
+                    }
+                    else
+                        Console.Write(fillCh);
+
+                }
+                Console.WriteLine();
+              
+            }
+            Console.WriteLine();
+            for (int k = 0; k < 5; k++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    Console.Write((k + x) % 2);
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+
+    class program
+    {
+        public static void Main(string[] args)
+        {
+           Rectangel.Rect(5, 100, '#', '*');
+            Console.WriteLine();
+            Chess.Ches();
+            Console.WriteLine();
+            Mix.SetMix(5 , 5 , '0' , '1');
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+    }
+}
